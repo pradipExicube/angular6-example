a804fb8ccb8611381e9ff4edf87f30a3b5875221
Pradip Mondal <pradip@Sudiptas-MacBook-Air.local>
2018-05-31 13:23:00 +0530

    firebase added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import * as firebase from 'firebase';
+
+var config = {
+  apiKey: "AIzaSyANjUwZ_Xw2ziVE4FTnAI1klvRsWvWGMDY",
+  authDomain: "angular6-fda86.firebaseapp.com",
+  databaseURL: "https://angular6-fda86.firebaseio.com",
+  projectId: "angular6-fda86",
+  storageBucket: "",
+  messagingSenderId: "668665359043"
+};
+firebase.initializeApp(config);
+
+
 
 @NgModule({
   declarations: [
